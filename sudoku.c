d0f74e1589330d634c25335673355b34e4094562
Replit user <>
2023-10-07 00:52:04 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,20 +70,6 @@ int is_valid(Node *n) {
     }
   }
 
-  for (int k = 0; k <= 9; k++) {
-    int matris[9] = {0};
-    for (int p = 0; p < 9; p++) {
-      int i = 3 * (k / 3) + (p / 3);
-      int j = 3 * (k % 3) + (p % 3);
-      int num = n->sudo[i][j];
-      if (num != 0) {
-        if (matris[num]) {
-          return 0;
-        }
-        matris[num] = 1;
-      }
-    }
-  }
 
   return 1;
 }
